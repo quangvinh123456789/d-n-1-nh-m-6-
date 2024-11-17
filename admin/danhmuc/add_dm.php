b1bee1c96bfc40716880e1136a3825f26d264fcd
A.Quan <trandinhanhquan17072021@gmail.com>
2024-11-18 00:14:58 +0700

    quan day code giao dien admin

--- admin/danhmuc/add_dm.php
+++ admin/danhmuc/add_dm.php
@@ -11,9 +11,9 @@
                     <label for="validationCustomUsername" class="form-label font-weight-bold">Tên danh mục</label>
                     <div class="input-group has-validation">
                         <input name="name" type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
-                        <!-- <div class="invalid-feedback">
+                        <div class="invalid-feedback">
                             Không được để trống
-                        </div> -->
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-6">
@@ -21,9 +21,9 @@
                     <div class="custom-file mb-3">
                         <input name="img" type="file" class="custom-file-input" id="validatedCustomFile" required>
                         <label class="custom-file-label" for="validatedCustomFile">Chọn tệp...</label>
-                        <!-- <div class="invalid-feedback">
+                        <div class="invalid-feedback">
                             Không được để trống
-                        </div> -->
+                        </div>
                     </div>
                 </div>
                 <div class="col-12 mt-1">
--- admin/index.php
+++ admin/index.php
@@ -1,41 +1,15 @@
 <?php
 ob_start();
 session_start();
-include 'header.php';
-include './model/pdo.php';
-include './model/danhmuc.php';
-include './model/sanpham.php';
-
-$list_dm = loadAll_danhmuc();
-
-if (isset($_GET['act']) && $_GET['act'] != '') {
+include "header.php";
+include "../model/pdo.php";
+// include "./global.php";
+if (isset($_GET['act']) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($act) {
-        case 'add_dm':{
-            if(isset($_POST['submit']) && $_POST['submit']){
-                $name = $_POST['name'];
-                $img = null;
-                $listone_dm = loadAll_danhmuc($name, 0);
-                if(is_array($listone_dm) && count($listone_dm) > 0){
-                    $err = "Danh mục đã tồn tại";
-                }
-                else{
-                    if ($_FILES['img']['name'] != ""){
-                        $img = time() . "_" . $_FILES['img']['name'];
-                        move_uploaded_file($_FILES['img']['tmp_name'], "../uploads/img_dm/$img");
-                    }
-                    insert_dm($name, $img);
-                    $thongbao = "Thêm danh mục thành công";
-                }
-            }
-            include 'danhmuc/add_dm.php';
-            break;
-        }
-        case 'list_dm':{
-            $list_dm = loadAll_danhmuc();
-            include 'danhmuc/list_dm.php';
-            break;
-        }
-        
-    }
-}gfcfyyhghygrtfyt
+            //quan ly danh muc
+} }else {
+    include "home.php";
+}
+include "footer.php";
+ob_end_flush();
